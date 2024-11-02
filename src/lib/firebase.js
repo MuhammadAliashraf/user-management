2f7938802a133e73338e2b5613b26bd06b41902d
Muhammad Ali <107420424+MuhammadAliashraf@users.noreply.github.com>
2024-11-02 22:58:45 +0500

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: 'AIzaSyD1soKpuF-9mPt1oMFkj81MnbqKaXiMEt0',
-  authDomain: 'user-data-a186f.firebaseapp.com',
-  projectId: 'user-data-a186f',
-  storageBucket: 'user-data-a186f.appspot.com',
-  messagingSenderId: '1007442068265',
-  appId: '1:1007442068265:web:8e2617a57e821ac78d6d38',
-  measurementId: 'G-YQL8YBC6D8',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
